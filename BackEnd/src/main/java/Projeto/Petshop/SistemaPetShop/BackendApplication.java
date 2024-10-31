11078c5fb30aa4a5d6e56c7d3f4b1a84946bf6bf
Mikaell Wilker <st4rz1n@gmail.com>
2024-10-31 16:43:33 -0300

    Adicionada a Conexão com o Postgress e Inicializado o Projeto estavel!

---
+++ BackEnd/src/main/java/Projeto/Petshop/SistemaPetShop/BackendApplication.java
@@ -0,0 +1,28 @@
+package Projeto.Petshop.SistemaPetShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API Backend Topicos Especiais",
+				version = "1.0",
+				description = "Trata-se da API que está sendo desenvolvida na disicplina TEPW",
+				contact = @Contact(
+						name = "Unichristus",
+						email = "adriano.candido@unichristus.edu.br",
+						url = "https://www.google.com")
+		)
+)
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
